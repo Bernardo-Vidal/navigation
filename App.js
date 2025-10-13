cf204c3ab60b6608dabf4708ca07428168d29424
Bernardo-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2025-10-13 10:42:12 -0300

    drawer

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+import Favoritos from './screens/Favoritos.js'
+import Locais from './screens/Locais.js'
+import Mapa from './screens/Mapa.js'
+import Mercadorias from './screens/Mercadorias.js'
+import Sobre from './screens/Sobre.js'
+
+const drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <drawer.Navigator>
+        <drawer.Screen name='Mapa' component={Mapa} />
+        <drawer.Screen name='Locais' component={Locais} />
+        <drawer.Screen name='Mercadorias' component={Mercadorias} />
+        <drawer.Screen name='Favoritos' component={Favoritos} />
+        <drawer.Screen name='Sobre' component={Sobre} />
+      </drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
