b02ed6bec7b7714e60666b5dc31fb1f9ba0e43c0
Bernardo-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2025-10-20 10:47:38 -0300

    mapa & sobre styles

--- style.js
+++ style.js
@@ -3,8 +3,29 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0077B6',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: '2.5%',
+    margin: '2.5%',
+    borderRadius: 10,
   },
+  sobreTitle: {
+    color: '#CAF0F8',
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  sobreSubTitle: {
+    color: '#CAF0F8',
+    fontSize: 30,
+    fontWeight: '400'
+  },
+  sobreText: {
+    color: '#CAF0F8',
+    fontSize: 20
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
 });
