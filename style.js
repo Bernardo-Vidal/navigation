b508b3250424ae1a5a3989c3819c1574a5f09b28
Vidal572 <bernardovidaldarosa@gmail.com>
2025-10-21 15:42:19 -0300

    Sobre

--- style.js
+++ style.js
@@ -1,31 +1,35 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0077B6',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    padding: '2.5%',
-    margin: '2.5%',
-    borderRadius: 10,
-  },
-  sobreTitle: {
-    color: '#CAF0F8',
-    fontSize: 40,
-    fontWeight: 'bold'
-  },
-  sobreSubTitle: {
-    color: '#CAF0F8',
-    fontSize: 30,
-    fontWeight: '400'
-  },
-  sobreText: {
-    color: '#CAF0F8',
-    fontSize: 20
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#0077B6",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        padding: "2.5%",
+        margin: "2.5%",
+        borderRadius: 10,
+    },
+    sobreTitle: {
+        color: "#CAF0F8",
+        fontSize: 40,
+        fontWeight: "bold",
+        marginVertical: "5%",
+    },
+    sobreSubTitle: {
+        color: "#CAF0F8",
+        fontSize: 30,
+        fontWeight: "400",
+        marginVertical: "5%",
+        marginHorizontal: ".5%",
+    },
+    sobreText: {
+        color: "#CAF0F8",
+        fontSize: 20,
+        marginHorizontal: "2%",
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
 });
